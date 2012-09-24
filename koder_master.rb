80db060658ef7aee3ee6704762c92a4ae3a4c140
Chad Arimura <carimura@gmail.com>
2012-09-23 17:33:53 -0700

    latest stuffs

--- koder_master.rb
+++ koder_master.rb
@@ -3,8 +3,6 @@ require 'iron_cache'
 require 'rest'
 require 'json'
 
-options = {}
-url = "https://api.stackexchange.com/2.1/users?order=desc&sort=reputation&site=stackoverflow"
 rest = Rest::Client.new(:gem => :typhoeus)
 cache = IronCache::Client.new(:token => params[:token], :project_id => params[:project_id])
 worker = IronWorkerNG::Client.new(:token => params[:token], :project_id => params[:project_id])
@@ -12,15 +10,21 @@ worker = IronWorkerNG::Client.new(:token => params[:token], :project_id => param
 koders = cache.cache("koders")
 user_ids = []
 
-res = rest.get(url, options)
-results = JSON.parse(res.body)
+pages = 5
 
-results["items"].each do |i|
-  puts "Found user #{i["display_name"]}"
-  koders.put(i["user_id"].to_s, i.to_s)
-  user_ids << i["user_id"]
+pages.downto(1) do |p|
+  url = "https://api.stackexchange.com/2.1/users?pagesize=100&order=desc&sort=reputation&site=stackoverflow&page=#{p}"
+  res = rest.get(url)
+  results = JSON.parse(res.body)
 
-  worker.tasks.create("koder_slave", :user_id => i["user_id"])
+  results["items"].each do |i|
+    puts "Found user #{i["display_name"]}"
+    koders.put(i["user_id"].to_s, i.to_s)
+    user_ids << i["user_id"]
+
+    worker.tasks.create("koder_slave", :user_id => i["user_id"])
+  end
 end
+
 
 puts "Processed #{user_ids.size} users"
